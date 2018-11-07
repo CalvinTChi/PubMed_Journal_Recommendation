31baf35e382a343b2d43c795e885427957d09eca
Calvin <calvin.t.chi@gmail.com>
2018-11-07 00:36:18 -0800

    train category

--- train_category.py
+++ train_category.py
@@ -1,5 +1,7 @@
-from keras.models import Sequential
 from keras.layers import Dense, Flatten, Embedding, Conv1D, MaxPooling1D, Activation
+from tensorflow.contrib.keras.api.keras.initializers import Constant
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
 from keras.utils import to_categorical
 import keras.optimizers
 import numpy as np
@@ -15,30 +17,20 @@ label_mapping = {'bioinformatics': 0, 'development': 1, 'epigenetics': 2, 'mende
 BATCH_SIZE = 512
 EMBEDDING_DIM = 200
 MAX_SEQ_LENGTH = 500
-VOCAB_SIZE = 
 global trainIterator
 trainIterator = pd.read_table("data/train.txt", delimiter="\t", header = 0, chunksize=BATCH_SIZE)
 trainIterator = iter(trainIterator)
+tokenizer = pickle.load(open("data/tokenizer.p", "rb"))
+embedding_matrix = pickle.load(open("data/embedding.p", "rb"))
 
 # INPUT: pandas df of rows x features1, where features = [abstract, PMID, category, journalAbbrev, impact_factor]
 # OUTPUT: (1) pandas df of rows x word2vec feature, (2) vector of labels in [0, 7].
 def generate_feature_label_pair(mat):
-    n = mat.shape[0]
-    X = np.zeros(shape = (n, 200))
-    Y = np.zeros(shape = n)
-    for i in range(n):
-        abstract = mat.iloc[i, 0].split(" ")
-        embedding = np.zeros(200)
-        nWords = 0
-        for word in abstract:
-            if word in word2vec:
-                vec = word2vec[word]
-                vec = vec / np.linalg.norm(vec)
-                embedding = np.add(embedding, vec)
-                nWords += 1  
-        X[i, :] = embedding / nWords
-        Y[i] = label_mapping[mat.iloc[i, 2]]
+    X = tokenizer.texts_to_sequences(mat.iloc[:, 0])
+    X = pad_sequences(X, maxlen = MAX_SEQ_LENGTH, padding='post')
     X = np.expand_dims(X, axis = 2)
+    Y = mat.iloc[:, 2].tolist()
+    Y = [label_mapping[label] for label in Y]
     Y = to_categorical(Y)
     return X, Y
 
@@ -53,11 +45,11 @@ def sample_generator():
         X, Y = generate_feature_label_pair(chunk)
         yield X, Y
 
-def create_model(tokenizer, embedding_matrix):
+def create_model():
     word_index = tokenizer.word_index
     embedding_layer = Embedding(len(word_index) + 1,
                                 EMBEDDING_DIM,
-                                weights = [embedding_matrix],
+                                embeddings_initializer = Constant(embedding_matrix),
                                 input_length = MAX_SEQ_LENGTH,
                                 trainable = False)
     model = Sequential()
@@ -83,9 +75,7 @@ def main():
     dev = pd.read_table("data/dev.txt", delimiter="\t", header = 0)
     devX, devY = generate_feature_label_pair(dev)
     nBatches = math.ceil(nTrain / batch_size)
-    tokenizer = pickle.load(open("data/tokenizer.p", "rb"))
-    embedding_matrix = pickle.load(open("data/embedding.p", "rb"))
-    model = create_model(tokenizer, embedding_matrix)
+    model = create_model()
     model.fit_generator(sample_generator(), steps_per_epoch = nBatches, epochs=2, validation_data=(devX, devY))
 
     model.save("model/category1")
