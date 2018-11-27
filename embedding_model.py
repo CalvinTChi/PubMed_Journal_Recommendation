388bc26436b9a7f60c90dde1ef36061da582db35
Ubuntu <ubuntu@ip-172-31-13-58.us-east-2.compute.internal>
2018-11-27 19:47:01 +0000

    working embedding_model

--- embedding_model.py
+++ embedding_model.py
@@ -1,15 +1,13 @@
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Flatten, Embedding, Conv1D, MaxPooling1D, Activation, Input, concatenate
 from tensorflow.contrib.keras.api.keras.initializers import Constant
 from keras.models import Model
 import tensorflow as tf
+from keras.backend import int_shape
 from keras.layers.normalization import BatchNormalization
 from keras.models import Sequential
 from keras.preprocessing.sequence import pad_sequences
-from keras.layers import concatenate
 from keras.models import load_model
 import keras.optimizers
-#from visualize_embedding import get_topic_embedding
-#from visualize_embedding import get_if_embedding
 from baseline import generate_feature_label_pair
 import math
 import numpy as np
@@ -22,6 +20,7 @@ MAX_SEQ_LENGTH = 500
 BATCH_SIZE = 512
 MAX_SEQ_LENGTH = 500
 EMBEDDING_SIZE = 256
+EMBEDDING_DIM = 200
 
 embedding_matrix = pickle.load(open("data/embedding.p", "rb"))
 tokenizer = pickle.load(open("data/tokenizer.p", "rb"))
@@ -63,10 +62,10 @@ def sample_generator():
             chunk = next(trainIterator)
         X, Y = generate_feature_label_pair(chunk)
         embedding = convert2embedding(X)
-        yield [X, embedding], Y
+        yield [np.array(X), np.array(embedding)], Y
 
 def create_model():
-    text_inputs = Input(shape = (MAX_SEQ_LENGTH, ))
+    text_inputs = Input(shape = (MAX_SEQ_LENGTH, ), name = "text_input")
     word_index = tokenizer.word_index
     embedding_layer = Embedding(len(word_index) + 1,
                                 EMBEDDING_DIM,
@@ -84,13 +83,14 @@ def create_model():
     x = Conv1D(128, 5, activation='relu')(x)
     x = BatchNormalization()(x)
     x = MaxPooling1D(35)(x)
+    x = Flatten()(x)
 
     embedding_input = Input(shape = (EMBEDDING_SIZE, ), name = "embedding_input")
     all_features = concatenate([x, embedding_input])
 
-    x = Dense(units=1200, activation='relu', input_shape=(all_features.output_shape,))(all_features)
+    x = Dense(units=1200, activation='relu', input_shape=(int_shape(all_features),))(all_features)
     x = BatchNormalization()(x)
-    x = Dense(units=800, activation='relu')(x)
+    x = Dense(units=1000, activation='relu')(x)
     x = BatchNormalization()(x)
     outputs = Dense(units=len(labelEncoder.classes_), activation = 'softmax')(x)
 
@@ -115,14 +115,13 @@ def main():
     # Get number of training samples
     with open("data/train_j.txt") as f:
         nTrain = sum(1 for _ in f)
-    #dev = pd.read_table("data/dev_j.txt", delimiter="\t", header = 0)
-    #devX, devY = generate_feature_label_pair(dev)
-    #devX = convert2embedding(devX)
-    #nBatches = math.ceil(nTrain / BATCH_SIZE)
+    dev = pd.read_table("data/dev_j.txt", delimiter="\t", header = 0)
+    devText, devY = generate_feature_label_pair(dev)
+    devEmbedding = convert2embedding(devText)
+    nBatches = math.ceil(nTrain / BATCH_SIZE)
     model = create_model()
-    print(model.summary())
-    #model.fit_generator(sample_generator(), steps_per_epoch = nBatches, epochs=2, validation_data=(devX, devY))
-    #model.save("model/embedding3.h5")
+    model.fit_generator(sample_generator(), steps_per_epoch = nBatches, epochs=2, validation_data=([devText, devEmbedding], devY))
+    model.save("model/embedding3.h5")
 
 if __name__ == "__main__":
     main()
