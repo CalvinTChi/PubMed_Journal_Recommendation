e47833259e42fa9c936fd882a94d9f6036f9f9d9
Ubuntu <ubuntu@ip-172-31-13-58.us-east-2.compute.internal>
2018-11-29 09:41:09 +0000

    evaluate

--- evaluate.py
+++ evaluate.py
@@ -1,32 +1,55 @@
 from keras.layers import Dense, Flatten, Embedding, Conv1D, MaxPooling1D, Activation, Input, concatenate, Dropout
-from keras.layers.normalization import BatchNormalization
-from keras.models import load_model, Model 
-from keras.preprocessing.sequence import pad_sequences
-from sklearn.metrics import accuracy_score, auc
-from embedding_model import create_model
 from tensorflow.contrib.keras.api.keras.initializers import Constant
-import matplotlib.pyplot as plt
+from keras.models import Model, load_model
 import tensorflow as tf
+from utils import *
+from keras.backend import int_shape
+from sklearn.metrics import accuracy_score, auc
+from keras.layers.normalization import BatchNormalization
+from keras.preprocessing.sequence import pad_sequences
+import keras.optimizers
+import matplotlib.pyplot as plt
+import math, pickle, sys
 import numpy as np
 import pandas as pd
-import pickle
-import math
-import sys
-import os
 
-BATCH_SIZE = 512
-EMBEDDING_DIM = 200
-MAX_SEQ_LENGTH = 500
-tokenizer = pickle.load(open("data/tokenizer.p", "rb"))
-labelEncoder = pickle.load(open("data/label_encoder.p", "rb"))
-embedding_matrix = pickle.load(open("data/embedding.p", "rb"))
+EMBEDDING_SIZE = 256
 
-def generate_feature_label_pair(mat):
-    X = tokenizer.texts_to_sequences(mat.iloc[:, 0])
-    X = pad_sequences(X, maxlen = MAX_SEQ_LENGTH, padding='post')
-    Y = mat.iloc[:, 3].tolist()
-    Y = labelEncoder.transform(Y)
-    return X, Y
+def create_embedding_model():
+    text_inputs = Input(shape = (MAX_SEQ_LENGTH, ), name = "text_input")
+    word_index = tokenizer.word_index
+    embedding_layer = Embedding(len(word_index) + 1,
+                                EMBEDDING_DIM,
+                                embeddings_initializer = Constant(embedding_matrix),
+                                input_length = MAX_SEQ_LENGTH,
+                                trainable = False)
+    x = embedding_layer(text_inputs)
+
+    # convolution 1st layer
+    x = Conv1D(128, 5, activation='relu', input_shape = (200, 1))(x)
+    x = BatchNormalization()(x)
+    x = MaxPooling1D(5)(x)
+
+    # convolution 2nd layer
+    x = Conv1D(128, 5, activation='relu')(x)
+    x = BatchNormalization()(x)
+    x = MaxPooling1D(35)(x)
+    x = Flatten()(x)
+
+    embedding_input = Input(shape = (EMBEDDING_SIZE, ), name = "embedding_input")
+    all_features = concatenate([x, embedding_input])
+
+    x = Dense(units=1000, activation='relu', input_shape=(int_shape(all_features),))(all_features)
+    x = BatchNormalization()(x)
+    x = Dropout(0.1)(x)
+    x = Dense(units=1000, activation='relu', input_shape=(int_shape(all_features),))(x)   
+    outputs = Dense(units=len(labelEncoder.classes_), activation = 'softmax')(x)
+
+    model = Model([text_inputs, embedding_input], outputs)
+    model.compile(loss = 'categorical_crossentropy',
+                 optimizer = keras.optimizers.Adam(lr=0.001), 
+                 metrics = ['accuracy'])
+    return model
 
 def rank_predictions(class_prob):
     return sorted(range(len(class_prob)), key=lambda i: class_prob[i], reverse=True)
@@ -70,11 +93,11 @@ def main(args):
         sys.exit(2)
     filename = args[0] + ".h5"
     test = pd.read_table("data/test_j.txt", delimiter="\t", header = 0)
-    testX, testY = generate_feature_label_pair(test)
+    testX, testY = generate_feature_label_pair(test, 3)
     if args[0][:-1] == "embedding":
         testEmbedding = convert2embedding(testX)
-        model = create_model()
-        model = model.load_weights("model/" + filename)
+        model = create_embedding_model()
+        model.load_weights("model/" + filename)
     else:
         model = load_model("model/" + filename)
 
@@ -126,6 +149,4 @@ if __name__ == "__main__":
         global if_graph
         if_graph = tf.get_default_graph()
     main(sys.argv[1:])
-
-
 
