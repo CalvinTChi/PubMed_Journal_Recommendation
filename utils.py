d96b5b486f62f337682f563df802134aa045d3c6
Ubuntu <ubuntu@ip-172-31-13-58.us-east-2.compute.internal>
2018-11-30 02:43:16 +0000

    utils

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import pickle
 
+EMBEDDING_SIZE = 256
 BATCH_SIZE = 512
 EMBEDDING_DIM = 200
 MAX_SEQ_LENGTH = 500
@@ -10,9 +11,9 @@ tokenizer = pickle.load(open("data/tokenizer.p", "rb"))
 labelEncoder = pickle.load(open("data/label_encoder.p", "rb"))
 embedding_matrix = pickle.load(open("data/embedding.p", "rb"))
 
-def generate_feature_label_pair(mat):
+def generate_feature_label_pair(mat, yIdx):
     X = tokenizer.texts_to_sequences(mat.iloc[:, 0])
     X = pad_sequences(X, maxlen = MAX_SEQ_LENGTH, padding='post')
-    Y = mat.iloc[:, 3].tolist()
+    Y = mat.iloc[:, yIdx].tolist()
     Y = labelEncoder.transform(Y)
     return X, Y
